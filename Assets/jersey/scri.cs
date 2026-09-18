371354806b0f96365a2c772c02bc9e1195693244
MUKILTEO\1614380 <scottbreiden60@gmial.com>
2026-09-18 08:28:04 -0700

    jeyssssssss

--- Assets/jersey/scri.cs
+++ Assets/jersey/scri.cs
@@ -6,11 +6,14 @@ public class scri : MonoBehaviour
     public float moveSpeed = 22f;
     Rigidbody2D rb;
     Transform target;
+    SpriteRenderer sprt;
+
     Vector2 moveDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprt = GetComponent<SpriteRenderer>();
     }
     void Start()
     {
@@ -25,14 +28,24 @@ public class scri : MonoBehaviour
             Vector3 direction = (target.position - transform.position).normalized;
             moveDirection = direction;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
+
+            //target.position.x
+            //sprt.flipX = true;
         }
     }
     private void FixedUpdate()
     {
-        if (target)
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            rb.linearVelocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+            if (target)
+            {
+                rb.linearVelocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+            }
         }
     }
 }
